19259f3760c960d8e9099e7cf48a4a9f039b1b0c
jcwyatt <jwyatt043@googlemail.com>
2016-07-13 22:39:19 +0100

    Playing around with the scrabble files to read from a dictionary and find the highest scoring word (scrabble2.py)

---
+++ scrabble2.py
@@ -0,0 +1,108 @@
+#program to give scrabble scores for words
+
+
+#funtion to read a list of words from a '\n' delimited file
+
+def getDict():
+
+	#with open('t8.shakespeare.txt', 'r') as f:
+	#with open('/usr/share/dict/american-english', 'r') as f:
+
+	with open('dictionary.txt', 'r') as f:
+		#raw1=f.read().replace('\'','')
+		#listOfWords = f.read.split()
+		listOfWords = f.read().splitlines()
+	f.closed
+	
+	return(listOfWords)
+
+#function to work out the scrabble word score
+
+def wordVal(word,vals): 
+	wordVal=0
+	for letter in word:
+		if letter in vals:
+			wordVal = wordVal + vals[letter]
+		else:
+			return ('error - must be letters')
+	return(wordVal)    
+
+
+#define letter values
+
+scrabLetVals={}
+for i in ('E','A','I','O','N','R','T','L','S','U'):
+	scrabLetVals[i] = 1
+for i in ('D','G'):
+	scrabLetVals[i] = 2
+for i in ('B','C','M','P'):
+	scrabLetVals[i] = 3
+for i in ('F','H','V','W','Y'):
+	scrabLetVals[i] = 4
+for i in ('K'):
+	scrabLetVals[i] = 5
+for i in ('J','X'):
+	scrabLetVals[i] = 8
+for i in ('Q','Z'):
+	scrabLetVals[i] = 10
+
+
+
+#define tile quantities
+
+scrabLetQty={'E':12,'A':9,'I':9,'O':8,'N':6,'R':6,'T':6,'L':4,'S':4,'U':4,
+			 'D':4, 'G':3,
+			 'B':2, 'C':2, 'M':2, 'P':2,
+			 'F':2, 'H':2, 'V':2, 'W':2, 'Y':2,
+			 'K':1, 'J':1, 'X':1, 'Q':1, 'Z':1
+			 }
+
+#main program
+
+dictionary = getDict()
+
+score = 0                   #initialise scores to non-zero amount
+topScore = 0				
+
+for word in dictionary:
+
+	status = 'ok'            # initialise status of validity of word
+
+	wordToCheck = str.upper(word)
+	#print (wordToCheck)
+	
+#is it valid (number of tiles in the bag)
+	try:
+		for letter in wordToCheck:
+			if wordToCheck.count(letter)>scrabLetQty[letter]:
+				#print('You used letter %s too many times, there are only %s of those tiles\n' % (letter, scrabLetQty[letter]))
+				status = 'void'
+				break
+	except KeyError:
+		#print('I got a key error here')
+		wordToCheck ='dogs'
+		status='void'
+
+#is it long enough (4 letter word or more)
+	if  0 < len(wordToCheck) < 4 :
+		#print('Words must be 4 or more characters\n')
+		status = 'void'
+
+
+	if status != 'void':
+		score = wordVal(wordToCheck,scrabLetVals)
+		#print ('score = ', score, '\n')
+		if score > topScore:
+			topScore = score
+			topWord = wordToCheck
+			print('Current High Score Word: %s \nWhich scores %s\n' % (topWord,topScore))
+			
+
+#if no word entered, exit the program
+		
+print('Thanks for using this app!\n')
+print('I got as far as %s' % word)
+print('The top score is %s' % topScore)
+print('The top word is %s' % topWord)
+
+	
